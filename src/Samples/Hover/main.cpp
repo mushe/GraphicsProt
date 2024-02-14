5c956338f64e4bda9060e757d67621a58537d711
mushe <mushegithub@gmail.com>
2024-02-15 01:50:54 +0900

    (WIP) Update hover display

--- src/Samples/Hover/main.cpp
+++ src/Samples/Hover/main.cpp
@@ -1,5 +1,21 @@
 #include "Core/Engine.h"
 
+struct Particle
+{
+    Vec2 position = Vec2(0.5f, 0.5f);
+    float lifeTime = 1.0f;
+    float age = 0.0f;
+};
+
+Particle SpawnParticle(Vec2 position)
+{
+    Particle particle;
+    particle.position = position;
+    particle.lifeTime = 1.0f;
+    particle.age = 0.0f;
+    return particle;
+}
+
 int main()
 {
     auto engine = Engine::Init(800, 800, 100, 50);
@@ -11,12 +27,16 @@ int main()
     float hoverForce = 8.0f;
     float rotationalForce = 40.0f;
     float rotationalVelocityAttenuation = 0.95f;
+    float particleSpawnRate = 1.0f;
 
     // variables
     Vec2 position = initialPosition;
     float rotation = 0.0f;
     float velocity = initialVelocity * 0.0001;;
     float rotationalVelocity = 0.0f;
+
+    // jet particle
+    std::vector<Vec2> particles;
 
     while (engine->Running())
     {
@@ -55,13 +75,31 @@ int main()
         Vec2 start = Vec2(cos(-rotation) * bodyLength, sin(-rotation) * bodyLength) + Vec2(position.x, 1.0f-position.y);
         Vec2 end = Vec2(-cos(-rotation) * bodyLength, -sin(-rotation) * bodyLength) + Vec2(position.x, 1.0f-position.y);
         ShapeDrawer::Line(start, end, 50.0f, Vec4(0.431, 0.561, 0.859, 1.0));
+        Vec4 jetColor = Vec4(0.6, 0.4, 0.3, 1.0);
+        ShapeDrawer::Triangle(start, Vec2(0, -1.0f), Vec2(0.1f, 0.05f), jetColor);
+        ShapeDrawer::Triangle(end, Vec2(0, -1.0f), Vec2(0.1f, 0.05f), jetColor);
+
+        
+        // particles
+        int particleAddCount = particleSpawnRate * (leftInput + rightInput);
+        for(int i = 0; i < particleAddCount; i++)
+        {
+            particles.push_back(Vec2(0.5f, 0.5f));
+        }
+        for(auto particle : particles)
+        {
+            ShapeDrawer::Rect(particle, Vec2(0.2f), Vec4(0.6, 0.4, 0.3, 1.0));
+        }
 
         // ui 
-        Text("rotationalVelocity : " + to_string(rotationalVelocity), Vec2(0.05, 0.75));
-        Text("rotation : " + to_string(rotation), Vec2(0.05, 0.8));
-        Text("press LEFT SHIFT and RIGHT SHIFT key to hover", Vec2(0.05, 0.85));
-        Text("position : " + to_string(position.x) + "," + to_string(position.y), Vec2(0.05, 0.9));
-        Text("left and right input : " + to_string(int(leftInput)) + "," + to_string(int(rightInput)), Vec2(0.05, 0.95));
+        Text("particle count : " + to_string(particles.size()), Vec2(0.05, 0.65));
+        Text("press LEFT SHIFT and RIGHT SHIFT key to hover", Vec2(0.05, 0.7));
+        Text("left and right input : " + to_string(int(leftInput)) + "," + to_string(int(rightInput)), Vec2(0.05, 0.75));
+
+        Text("position : " + to_string(position.x) + "," + to_string(position.y), Vec2(0.05, 0.80));
+        Text("velocity : " + to_string(velocity) , Vec2(0.05, 0.85));
+        Text("rotation : " + to_string(rotation), Vec2(0.05, 0.9));
+        Text("rotationalVelocity : " + to_string(rotationalVelocity), Vec2(0.05, 0.95));
         engine->OnGUI([&]()
         {
             GUI::Float(hoverForce, "hover force", 0.1f);
