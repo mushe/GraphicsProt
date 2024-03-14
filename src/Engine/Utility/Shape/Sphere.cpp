23a004beffba9801c86288e4fa6380f38d71fa63
mushe <mushegithub@gmail.com>
2024-03-15 01:23:10 +0900

    Implement sphere to sphere intersection

--- src/Engine/Utility/Shape/Sphere.cpp
+++ src/Engine/Utility/Shape/Sphere.cpp
@@ -1,6 +1,13 @@
 #include "Sphere.h"
 
-bool Sphere::Intersect(const Shape& other)
-{
-    return true;
+// todo 
+// apply visitor pattern or other good design pattern
+bool Sphere::Intersect(const Shape& other) {
+
+  auto otherSphere = dynamic_cast<const Sphere*>(&other);
+  if(otherSphere) {
+    return length((otherSphere->pos_ - pos_)) < otherSphere->radius_ + radius_;
+  }
+
+  return false;
 }
